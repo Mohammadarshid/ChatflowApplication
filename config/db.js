53783a2f4f7c0c36951def5c02a31b7d8f65d85d
mohammadarshid <mohammadarshid3@gmail.com>
2026-04-02 15:56:14 +0530

    file change response  database file

--- config/db.js
+++ config/db.js
@@ -1,16 +1,17 @@
-const mongoose =require("mongoose");
-const  colors  =require('colors')
+const mongoose = require("mongoose");
+const colors = require('colors')
+
 //  function mongodb database cnnection
 
 const connectdb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL), 
-        console.log(`connected to database:${mongoose.connection.host}`.bgBlue.white)
-     
+        await mongoose.connect(process.env.MONGO_URL),
+            console.log(`connected to database Successfully:${mongoose.connection.host}`.bgBlue.green)
+
     } catch (error) {
-      console.log( 'db error', error,colors.bgYellow );
-              
+        console.log('db error', error, colors.bgYellow);
+
     }
-} 
+}
 
 module.exports = connectdb
